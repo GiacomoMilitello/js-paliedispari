a363108272a20e41abebe01390639af22398bf32
GiacomoMilitello <g.militello95@gmail.com>
2024-01-15 20:21:24 +0100

    pari/dispari js

--- pari e dispari/assets/js/main.js
+++ pari e dispari/assets/js/main.js
@@ -6,3 +6,33 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+
+const buttonOk = document.getElementById("ok");
+var numberHtml = document.getElementById('number');
+var pariDispari = document.getElementById('pari-dispari');
+const result = document.getElementById('result');
+
+function randomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+buttonOk.addEventListener('click', function() {
+    let numberPc = randomNumber();
+    var sum = parseInt(numberHtml.value) + numberPc;
+    var even = isEven(sum);
+
+    if (numberHtml.value >= 1 && numberHtml.value <= 5) {
+        if ((even && pariDispari.value === 'pari') || (!even && pariDispari.value === 'dispari')) {
+            result.innerHTML = 'hai vinto!! La Somma tra il tuo numero ' + numberHtml.value + ' ed il numero randomico ' + numberPc + ' è: ' + sum;
+        } else {
+            result.innerHTML = 'hai perso!! La Somma tra il tuo numero ' + numberHtml.value + ' ed il numero randomico ' + numberPc + ' è: ' + sum;
+        }
+    } else {
+        result.innerHTML = 'Per favore inserisci un numero compreso tra 1 e 5.';
+    }
+})
