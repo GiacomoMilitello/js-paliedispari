8fbbd6af160782c46a2611821f955b5c92855761
GiacomoMilitello <g.militello95@gmail.com>
2024-01-15 21:55:58 +0100

    palindroma starting js

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -3,3 +3,20 @@ Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+const buttonOk = document.getElementById("ok");
+var wordHtml = document.getElementById('word');
+const result = document.getElementById('result');
+
+function palindrome() {
+    /// !!!!!!!!!!!!!!!!!
+}
+
+buttonOk.addEventListener('click', function() {
+    const word = wordHtml.value;
+    if(palindrome()) {
+        result.textContent = "La parola " + wordHtml.value + " è palindroma";
+    } else {
+        result.textContent = "La parola " + wordHtml.value + " non è palindroma";
+    }
+});
