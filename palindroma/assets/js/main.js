e6a3114dc2fd1a1903f8f9576a374d4118d33239
GiacomoMilitello <g.militello95@gmail.com>
2024-01-16 14:47:39 +0100

    palindroma js

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -8,13 +8,17 @@ const buttonOk = document.getElementById("ok");
 var wordHtml = document.getElementById('word');
 const result = document.getElementById('result');
 
-function palindrome() {
-    /// !!!!!!!!!!!!!!!!!
+function palindrome( word ) {
+    let parolaAlContrario = ''
+    for (let i = word.length - 1; i >= 0; i--){
+        parolaAlContrario += word[i]
+    }
+    return parolaAlContrario
 }
 
 buttonOk.addEventListener('click', function() {
     const word = wordHtml.value;
-    if(palindrome()) {
+    if(word === palindrome(word)) {
         result.textContent = "La parola " + wordHtml.value + " è palindroma";
     } else {
         result.textContent = "La parola " + wordHtml.value + " non è palindroma";
